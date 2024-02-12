152dba28115d0c5bcabeea0fb5dec6192fb730ff
edemy5 <edem.amankwah@gmail.com>
2024-02-11 20:42:42 -0800

    models

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+"""
+Module Representing the Amenity Class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Class representing amenity
+    Attributes: name
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+Module for State class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Class representing a state with attribute: name
+    """
+    name = ""
